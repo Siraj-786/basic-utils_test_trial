48f50dff870a5273f79d11164eb2137cb4df25f0
Siraj Shaik <siraj.shaik@juspay.in>
2025-07-01 12:06:24 +0530

    this should fail and this is trial 5 dublicated

--- basic_utils/tests/test_core.py
+++ basic_utils/tests/test_core.py
@@ -7,7 +7,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 from core import reverse_string, is_even, maximum, list_sum
 
 def test_reverse_string():
-    assert reverse_string("hello") == "olleh this is trial 4 i guess "
+    assert reverse_string("hello") == "olleh"
+    assert reverse_string("") == ""
     assert reverse_string("") == ""
 
 def test_is_even():
